691c248002e8bc94901008d7e94e4d0728365b80
Michel <orphee@gmail.com>
2013-03-28 11:52:58 -0700

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ rethinkdb-driver-development
 ============================
 
 Tool to help users create a new driver for RethinkDB.
+A quick and dirty fork of RethinkDB's python driver.
+
+Run `pyhon test.py`
+Specify the host and driver port of your RethinkDB instance
+Write a query and get back
+- The message
+- The serialized message
+- The serialized response from the server
+- The response from the server
+
+Dependency
+----
+RethinkDB
+
+Install
+----
+git clone git@github.com:neumino/rethinkdb-driver-development
+
+Run
+----
+python test.py
---
+++ test.py
@@ -0,0 +1,38 @@
+import rethinkdb as r
+import sys
+
+print "Host (default value is 'localhost'):"
+host = raw_input()
+if host == '':
+    host = 'localhost'
+
+print "Driver port (default value is 28015):"
+port_raw = raw_input()
+if port_raw is '':
+    port = 28015
+else:
+    port = int(port_raw)
+    
+print "Connecting to the server..."
+c = r.connect(host=host, port=port)
+print "Connected."
+
+while True:
+    try:
+        print "----------------------------------------"
+        print "Enter your query (without using .run()):"
+        query_raw = raw_input()
+        if query_raw == 'exit':
+            sys.exit(0)
+
+        query = eval(query_raw)
+        query.run(c)
+    except (KeyboardInterrupt, SystemExit):
+        print
+        print "Bye bye"
+        sys.exit(0)
+    except:
+        e = sys.exc_info()
+        for line in e:
+            print str(line)
+    print
